b8eb71ea5119b5b2e2753fd9124be6a037591ecb
Kyle Adkins <kyle.m.adkins@gmail.com>
2024-09-26 01:48:42 -0400

    Draw and rotate player

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill((0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
